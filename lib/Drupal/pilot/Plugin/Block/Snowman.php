498b3a14a50bd616004582de2fb80f1cc6f4b528
Helior Colorado <me@helior.info>
2014-01-23 01:03:07 -0800

    Added a snowman block plugin.

---
+++ lib/Drupal/pilot/Plugin/Block/Snowman.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\pilot\Plugin\Block\Snowman.
+ */
+
+namespace Drupal\pilot\Plugin\Block;
+
+use Drupal\block\BlockBase;
+
+/**
+ * Provides a 'Snowman' block.
+ *
+ * @Block(
+ *   id = "pilot_snowman",
+ *   admin_label = @Translation("Snowman unicode character")
+ * )
+ */
+class Snowman extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return array('#markup' => '<span style="font-size:12em;"">☃</span>');
+  }
+}
