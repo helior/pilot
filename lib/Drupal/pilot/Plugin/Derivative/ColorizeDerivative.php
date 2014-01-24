b995ff27181be55105fac4f426c99c1b1424436f
Helior Colorado <me@helior.info>
2014-01-24 02:25:41 -0800

    Fixed ids for colorize derivatives.

--- lib/Drupal/pilot/Plugin/Derivative/ColorizeDerivative.php
+++ lib/Drupal/pilot/Plugin/Derivative/ColorizeDerivative.php
@@ -8,7 +8,6 @@
 namespace Drupal\pilot\Plugin\Derivative;
 
 use Drupal\Component\Plugin\Derivative\DerivativeBase;
-use Drupal\Core\Plugin\Discovery\ContainerDerivativeInterface;
 
 /**
  * Provides colorize image effect plugin definitions for all supported colors.
@@ -23,6 +22,9 @@ class ColorizeDerivative extends DerivativeBase {
   		$this->derivatives[$name] = $base_plugin_definition;
   		$this->derivatives[$name]['label'] = t('Colorize to @color', array('@color' => $info['title']));
   		$this->derivatives[$name]['colors'] = $info['colors'];
+
+  		// Normally I probably wouldn't have to do this, but I think Core is borked.
+  		$this->derivatives[$name]['id'] = 'pilot_colorize:' . $name;
   	}
 
     return $this->derivatives;
